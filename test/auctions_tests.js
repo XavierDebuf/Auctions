23bad602daf9c602a7cf9d1049c97378525d8299
XavierDebuf <x.debuf@gmail.com>
2023-09-20 15:25:29 +0200

    test unitaire de deploiement ok

--- test/auctions_tests.js
+++ test/auctions_tests.js
@@ -5,9 +5,13 @@ const auctionsTests = artifacts.require("../contracts/Auction.sol");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("auctionsTests", function (/* accounts */) {
-  it("should assert true", async function () {
-    await auctionsTests.deployed();
-    return assert.isTrue(true);
+contract("auctionsTests", function () {
+  it("should assert address", async function () {
+   const auction = await auctionsTests.deployed();
+   console.log(auction.address)
+   assert(auction.address != "" && auction.address!=undefined)
   });
+  //it("should assert auction registered ok") async function(){
+  //  await auctionsTests.
+ // }
 });
